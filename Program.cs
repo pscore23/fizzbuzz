7d83c16ace9cf179e2bfe65555779e2c8df07d45
pscore23 <rubik2357x@gmail.com>
2022-09-28 19:10:55 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FizzBuzz {
+    public static class Program {
+        public static void Main(string[] args) {
+            for (int i = 1; i <= 100000; i++) {
+                Console.WriteLine(Calc(i));
+            }
+        }
+
+        private static string Calc(int n) {
+            string is_fb = "";
+
+            if (n % 3 == 0) {
+                is_fb+= "Fizz";
+            }
+
+            if (n % 5 == 0) {
+                is_fb+= "Buzz";
+            }
+
+            return is_fb == "" ? n.ToString() : is_fb;
+        }
+    }
+}
